bbec2914cf52b4a873d2c4ba1b576b263defb02e
sardaryanh <sardaryanhrant@gmail.com>
2019-12-02 12:33:26 +0400

    constructor design pattern

---
+++ creational/constructor.js
@@ -0,0 +1,30 @@
+// Before
+// function Server(name, ip) {
+//     this.name = name
+//     this.ip =ip
+
+// }
+
+// Server.prototype.getUrl = function () {
+//     return `https://${this.ip}:80`
+// }
+// const aws = new Server('AWS German', '82.21.21.32')
+// console.log(aws.getUrl());
+
+
+
+// Now
+class Server {
+    constructor(name, ip) {
+        this.name = name
+        this.ip = ip
+    }
+
+    getUrl = function () {
+        return `https://${this.ip}:80`
+    }
+}
+
+const aws = new Server('AWS German', '82.21.21.32')
+
+console.log(aws.getUrl());
